5468722424ea6391727191c4f518b82d470d55cf
Ayfri <pierre.ayfri@gmail.com>
2020-11-26 11:40:11 +0100

    Added checkSize method in Embed class.

--- src/utils/Embed.js
+++ src/utils/Embed.js
@@ -51,4 +51,32 @@ module.exports = class Embed extends MessageEmbed {
 
 		return new Embed(setValues(template, values));
 	}
+
+	checkSize() {
+		const limits = {
+			author: {
+				name: 256,
+			},
+			title: 256,
+			description: 2048,
+			footer: {
+				text: 2048,
+			},
+			fields: {
+				size: 25,
+				name: 256,
+				value: 1024,
+			},
+		};
+
+		if (this.title?.length > limits.title) throw new RangeError(`embed.title is too long (${limits.title}).`);
+		if (this.author?.name.length > limits.author.name) throw new RangeError(`embed.author.name is too long (${limits.author.name}).`);
+		if (this.description?.length > limits.description) throw new RangeError(`embed.description is too long (${limits.description}).`);
+		if (this.title?.length > limits.title) throw new RangeError(`embed.title is too long (${limits.title}).`);
+		if (this.fields?.length > limits.fields.size) throw new RangeError(`Too much fields is too long (${limits.fields.size}).`);
+		this.fields.forEach(field => {
+			if (field.name?.length > limits.fields.name) throw new RangeError(`embed.fields[${this.fields.indexOf(field)}].name is too long (${limits.fields.name}).`);
+			if (field.value?.length > limits.fields.value) throw new RangeError(`embed.fields[${this.fields.indexOf(field)}].value is too long (${limits.fields.value}).`);
+		});
+	}
 };
