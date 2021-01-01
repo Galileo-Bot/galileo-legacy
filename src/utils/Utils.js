c9d9159e7f66c8aaf78dc1bc4191fd12f978a818
Ayfri <pierre.ayfri@gmail.com>
2021-01-01 04:42:07 +0100

    Fixed JSDoc type for sendLogMessage function.

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -48,7 +48,7 @@ function random(array) {
  * Permet d'envoyer un message de log sur le serveur de gali.
  * @param {GaliClient | Client} client - Le client pour récupérer les salons etc.
  * @param {'addGuild' | 'bug' | 'command' | 'mp' | 'removeGuild' | 'rateLimit'} channelType - Type de salon.
- * @param {string | module:"discord.js".StringResolvable | Embed} content - Le contenu.
+ * @param {BetterEmbed | string} content - Le contenu.
  * @returns {void}
  */
 async function sendLogMessage(client, channelType, content) {
