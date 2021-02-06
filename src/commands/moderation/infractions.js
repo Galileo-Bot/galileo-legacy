e2d896c7ca370dcef9342571ac3b656efa795d9e
Ayfri <pierre.ayfri@gmail.com>
2021-02-06 18:55:54 +0100

    Fixed error when trying to unban an user still there.

--- src/commands/moderation/infractions.js
+++ src/commands/moderation/infractions.js
@@ -74,10 +74,12 @@ module.exports = class InfractionsCommand extends Command {
 
 		if (args[1] === 'supprimer') {
 			if (['toutes', 'all', 'tout'].includes(args[2])) {
-				if (this.client.dbManager.userInfos.get(message.guild.id, person.user.id).sanctions.find(s => s.type === 'ban')) {
-					message.guild.members
-						.unban(person.user.id)
-						.catch(() => super.send('Une erreur a eu lieu dans le débanissement de ce membre, vérifiez que le bot ait la permission de bannir les membres.'));
+				if (this.client.dbManager.userInfos.get(message.guild.id, person.user.id).sanctions.find(s => s.type === 'ban') && (await message.guild.fetchBan(person))) {
+					try {
+						await message.guild.members.unban(person.user.id);
+					} catch (ignored) {
+						await super.send('Une erreur a eu lieu dans le débanissement de ce membre, vérifiez que le bot ait la permission de bannir les membres.');
+					}
 				}
 
 				this.client.dbManager.userInfos.set(message.guild.id, [], `${person.user.id}.sanctions`);
@@ -94,7 +96,7 @@ module.exports = class InfractionsCommand extends Command {
 				if (sanction) {
 					if (sanction.type === 'ban') message.guild.members.unban(person.user.id).catch(() => {});
 					this.client.dbManager.userInfos.remove(message.guild.id, s => s === sanction, `${person.user.id}.sanctions`);
-					return super.send(`La sanction ${args[2]} a bien été supprimée. \`(${sanction.type})\``);
+					return super.send(`La sanction ${args[2]} de ${person} a bien été supprimée. \`(${sanction.type})\``);
 				} else return argError(message, this, `La sanction ${args[2]} n'a pas été trouvée ou n'est pas valide.`);
 			}
 		} else if (args[1] === 'modifier') {
