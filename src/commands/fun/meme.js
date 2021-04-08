9369d71563e3e3806908084c8a6ff5f95b53cc07
Ayfri <pierre.ayfri@gmail.com>
2021-04-08 11:51:21 +0200

    feat: Amélioration de la commande meme, de la description si un mème n'est pas trouvé, et de la recherche de mèmes.

--- src/commands/fun/meme.js
+++ src/commands/fun/meme.js
@@ -1,35 +1,52 @@
 const Command = require('../../entities/Command.js');
 const {random} = require('../../utils/Utils.js');
 const fetch = require('node-fetch');
+const EvalCommand = require('../owner/eval.js');
 const {BetterEmbed} = require('discord.js-better-embed');
 module.exports = class MemeCommand extends Command {
 	constructor() {
 		super({
 			aliases: ['mème', 'même', 'reddit'],
-			description: "Envoie un mème aléatoire depuis le subreddit 'meme' ou celui inscrit.",
+			description: 'Envoie un mème aléatoire depuis le subreddit \'meme\' ou celui inscrit.',
 			name: 'meme',
-			usage: 'meme\nmeme <subreddit>',
+			usage: 'meme\nmeme <subreddit>'
 		});
 	}
 
 	static async getRandomMemeFromSubreddit(subreddit) {
-		const image = random((await (await fetch(`https://imgur.com/r/${subreddit}/hot.json`)).json()).data);
+		/**
+		 * @typedef {Object} Image
+		 * @property {boolean} animated
+		 * @property {string} hash
+		 * @property {string} ext
+		 * @property {string} mimetype
+		 * @property {boolean} in_gallery
+		 * @property {object} adConfig
+		 * @property {string[]} adConfig.safeFlags
+		 */
+		/**
+		 * @type {Image[]}
+		 */
+		const images = (await (await fetch(`https://imgur.com/r/${subreddit}/hot.json`)).json()).data.filter(image => image.mimetype && image.mimetype.startsWith('image'));
+		const image = random(images);
 		return image ? `https://imgur.com/${image.hash}${image.ext.replace(/\?.*/, '')}` : null;
 	}
 
 	async run(client, message, args) {
 		await super.run(client, message, args);
 		const subreddit = args[0] ? args[0].toLowerCase() : 'meme';
-
-		const meme = await MemeCommand.getRandomMemeFromSubreddit(subreddit);
-		const embed = BetterEmbed.fromTemplate('image', {
-			client,
-			description: `Mème venant du [subreddit \`${subreddit}\`](https://www.reddit.com/r/${subreddit}).\n${
-				meme ? '' : `(Subreddit \`${subreddit}\` non trouvé.)`
-			}\n\n[Cliquez ici pour avoir le lien.](${meme})`,
-			image: meme ?? (await MemeCommand.getRandomMemeFromSubreddit('meme')),
-			title: 'Voici votre mème :',
+		const embed = BetterEmbed.fromTemplate('basic', {
+			client
 		});
+
+		let meme = (await MemeCommand.getRandomMemeFromSubreddit(subreddit));
+		embed.setDescription(`Mème venant du [subreddit \`${meme ? subreddit : 'meme'}\`](https://www.reddit.com/r/${subreddit}).
+${meme ? '' : `(Subreddit \`${subreddit}\` non trouvé, ou pas d'image trouvée.)\n`}
+[Cliquez ici pour avoir le lien.](${meme})`);
+		embed.setTitle('Voici votre mème :');
+		meme ??= (await MemeCommand.getRandomMemeFromSubreddit('meme'));
+		embed.setImage(meme);
+		embed.setURL(meme);
 
 		await super.send(embed);
 	}
